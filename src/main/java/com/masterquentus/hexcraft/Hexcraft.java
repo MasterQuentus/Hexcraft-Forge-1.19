7a42d3baa23b6c1e2ec408b940b7b8feb0bd60f5
Dalto <DaltonRPratt@gmail.com>
2022-06-27 14:40:37 -0500

    Initial commit

---
+++ src/main/java/com/masterquentus/hexcraft/Hexcraft.java
@@ -0,0 +1,39 @@
+package com.masterquentus.hexcraft;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+@Mod(Hexcraft.MOD_ID)
+public class Hexcraft
+{
+    public static final String MOD_ID = "hexcraft";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Hexcraft()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
