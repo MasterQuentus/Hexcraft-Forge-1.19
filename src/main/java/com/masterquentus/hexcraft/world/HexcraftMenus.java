d42af741f347a8e46fe73dd5ef63e94840d7eba5
Dalto <DaltonRPratt@gmail.com>
2022-07-17 22:20:58 -0500

    added Crates

--- src/main/java/com/masterquentus/hexcraft/world/HexcraftMenus.java
+++ src/main/java/com/masterquentus/hexcraft/world/HexcraftMenus.java
@@ -25,14 +25,18 @@ public class HexcraftMenus {
 
     private static final DeferredRegister<MenuType<?>> REGISTRY = DeferredRegister.create(ForgeRegistries.MENU_TYPES, Hexcraft.MOD_ID);
 
-    public static final RegistryObject<MenuType<Crate_WhiteOakGUIMenu>> WHITE_OAK_CRATE_GUI = REGISTRY.register("white_oak_crate_gui",
-            () -> new MenuType<>((id, inv) -> new Crate_WhiteOakGUIMenu(id, inv, null)));
-    public static final RegistryObject<MenuType<Crate_HellbarkGUIMenu>> HELLBARK_CRATE_GUI = REGISTRY.register("hellbark_crate_gui",
-            () -> new MenuType<>((id, inv) -> new Crate_HellbarkGUIMenu(id, inv, null)));
+    //  public static final MenuType<EbonyCrateGUIMenu> EBONY_CRATE_GUI = register("ebony_crate_gui",
+//            (id, inv, extraData) -> new EbonyCrateGUIMenu(id, inv, extraData));
+
     public static final RegistryObject<MenuType<Crate_BloodOakGUIMenu>> BLOOD_OAK_CRATE_GUI = REGISTRY.register("blood_oak_crate_gui",
             () -> new MenuType<>((id, inv) -> new Crate_BloodOakGUIMenu(id, inv, null)));
-//  public static final MenuType<EbonyCrateGUIMenu> EBONY_CRATE_GUI = register("ebony_crate_gui",
-//            (id, inv, extraData) -> new EbonyCrateGUIMenu(id, inv, extraData));
+
+    public static final RegistryObject<MenuType<Crate_HellbarkGUIMenu>> HELLBARK_CRATE_GUI = REGISTRY.register("hellbark_crate_gui",
+            () -> new MenuType<>((id, inv) -> new Crate_HellbarkGUIMenu(id, inv, null)));
+
+    public static final RegistryObject<MenuType<Crate_WhiteOakGUIMenu>> WHITE_OAK_CRATE_GUI = REGISTRY.register("white_oak_crate_gui",
+            () -> new MenuType<>((id, inv) -> new Crate_WhiteOakGUIMenu(id, inv, null)));
+
 
     public static void register(IEventBus eventBus) {
         REGISTRY.register(eventBus);
