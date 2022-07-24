6a32a52f9ad31764b6e0d2d5fbf8068cc5cc26ce
Dalto <DaltonRPratt@gmail.com>
2022-07-23 19:34:49 -0500

    fixed amethyst chimes

--- src/main/java/com/masterquentus/hexcraft/block/custom/AmethysChimesBlock.java
+++ src/main/java/com/masterquentus/hexcraft/block/custom/AmethysChimesBlock.java
@@ -9,6 +9,8 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 import java.util.Collections;
 import java.util.List;
@@ -17,6 +19,8 @@ public class AmethysChimesBlock extends Block {
     public AmethysChimesBlock(Properties sound) {
         super(BlockBehaviour.Properties.of(Material.AMETHYST).sound(SoundType.AMETHYST).strength(1f, 10f));
     }
+
+    private static final VoxelShape SHAPE =  Block.box(1, 1, 1, 16, 12, 16);
 
     @Override
     public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
@@ -29,4 +33,10 @@ public class AmethysChimesBlock extends Block {
             return dropsOriginal;
         return Collections.singletonList(new ItemStack(this, 1));
     }
+
+    @Override
+    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        return SHAPE;
+    }
+
 }
