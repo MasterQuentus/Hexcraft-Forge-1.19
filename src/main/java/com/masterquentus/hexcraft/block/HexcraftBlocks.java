08bc7cc997e5b54a634dd709543f63ae42a0ce8f
MasterQuentus <DaltonRpratt@gmail.com>
2023-05-29 23:52:57 -0500

    added Sirens And Mermaids

--- src/main/java/com/masterquentus/hexcraft/block/HexcraftBlocks.java
+++ src/main/java/com/masterquentus/hexcraft/block/HexcraftBlocks.java
@@ -1773,6 +1773,7 @@ public class HexcraftBlocks {
             () -> new DoorBlock(BlockBehaviour.Properties.of(Material.WOOD)
                     .strength(3.0F).noOcclusion()), HexcraftCreativeModeTab.HEXCRAFT_TAB);
 
+
     //Trapdoors
     public static final RegistryObject<Block> EBONY_TRAPDOOR = registerBlock("ebony_trapdoor",
             () -> new TrapDoorBlock(BlockBehaviour.Properties.of(Material.WOOD)
