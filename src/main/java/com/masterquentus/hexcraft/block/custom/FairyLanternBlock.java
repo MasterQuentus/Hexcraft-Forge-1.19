d9e796139948709c8ffd614ae37d10bba01c85f0
HauntedCorpse <bls3tacos@gmail.com>
2023-06-16 08:28:53 -0500

    ✨ Fairies are now attracted to Fairy Lanterns

--- src/main/java/com/masterquentus/hexcraft/block/custom/FairyLanternBlock.java
+++ src/main/java/com/masterquentus/hexcraft/block/custom/FairyLanternBlock.java
@@ -1,25 +1,27 @@
 package com.masterquentus.hexcraft.block.custom;
 
+import com.masterquentus.hexcraft.block.entity.HexcraftBlockEntities;
+import com.masterquentus.hexcraft.block.entity.custom.FairyLanternBlockEntity;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.LanternBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-public class FairyLanternBlock extends LanternBlock {
+public class FairyLanternBlock extends LanternBlock implements EntityBlock {
     public static final BooleanProperty LIT = BooleanProperty.create("lit");
 
     public FairyLanternBlock(Properties properties) {
@@ -37,5 +39,15 @@ public class FairyLanternBlock extends LanternBlock {
         super.appendHoverText(stack, level, components, flag);
     }
 
+    @Nullable
+    @Override
+    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+        return new FairyLanternBlockEntity(pos, state);
+    }
 
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level pLevel, BlockState pState, BlockEntityType<T> pBlockEntityType) {
+        return pBlockEntityType == HexcraftBlockEntities.FAIRY_LANTERN.get() ? FairyLanternBlockEntity::tick : null;
+    }
 }
