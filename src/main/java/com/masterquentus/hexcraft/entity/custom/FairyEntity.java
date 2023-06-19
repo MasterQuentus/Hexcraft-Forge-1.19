88a7df0f7387e3508e2fdee40d410bb83206ec53
HauntedCorpse <bls3tacos@gmail.com>
2023-06-19 10:53:38 -0500

    📦 Updated Fairy Attraction To Fairy Lantern
    It should work better now.

--- src/main/java/com/masterquentus/hexcraft/entity/custom/FairyEntity.java
+++ src/main/java/com/masterquentus/hexcraft/entity/custom/FairyEntity.java
@@ -15,10 +15,12 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.control.MoveControl;
 import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
+import net.minecraft.world.entity.ai.goal.MoveToBlockGoal;
 import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
@@ -30,6 +32,7 @@ import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 
+import javax.print.DocFlavor;
 import java.util.EnumSet;
 
 public class FairyEntity extends FlyingMob implements IAnimatable {
@@ -51,6 +54,7 @@ public class FairyEntity extends FlyingMob implements IAnimatable {
     }
 
     protected void registerGoals() {
+        this.goalSelector.addGoal(1, new FairyMoveToLanternGoal(this));
         this.goalSelector.addGoal(3, new LookAtPlayerGoal(this, Player.class, 8.0F));
         this.goalSelector.addGoal(5, new fairyMoveAroundGoal(this));
         this.goalSelector.addGoal(5, new RandomLookAroundGoal(this));
@@ -187,21 +191,39 @@ public class FairyEntity extends FlyingMob implements IAnimatable {
             double d0 = this.fairyEntity.getX() + (double)((randomsource.nextFloat() * 2.0F - 1.0F) * 16.0F);
             double d1 = this.fairyEntity.getY() + (double)((randomsource.nextFloat() * 2.0F - 1.0F) * 16.0F);
             double d2 = this.fairyEntity.getZ() + (double)((randomsource.nextFloat() * 2.0F - 1.0F) * 16.0F);
-            
-            if (this.fairyEntity.lanternPos != null) {
-                boolean lanternExists = this.fairyEntity.level.getBlockState(fairyEntity.lanternPos).getBlock()
-                        .equals(HexcraftBlocks.FAIRY_LANTERN.get());
 
-                if (lanternExists && fairyEntity.distanceToSqr(fairyEntity.lanternPos.getX() + 0.5,
-                        fairyEntity.lanternPos.getY() + 0.5, fairyEntity.lanternPos.getZ() + 0.5) > 8.0) {
-                    this.fairyEntity.getMoveControl().setWantedPosition(fairyEntity.lanternPos.getX() + 0.5,
-                            fairyEntity.lanternPos.getY()  + 0.5, fairyEntity.lanternPos.getZ()  + 0.5, 1.0D);
-                } else {
-                    this.fairyEntity.lanternPos = null;
-                }
+            this.fairyEntity.getMoveControl().setWantedPosition(d0, d1, d2, 1.0D);
+        }
+    }
+
+    static class FairyMoveToLanternGoal extends Goal {
+        private final FairyEntity fairyEntity;
+
+        public FairyMoveToLanternGoal(FairyEntity pFairy) {
+            this.fairyEntity = pFairy;
+            this.setFlags(EnumSet.of(Goal.Flag.MOVE));
+        }
+
+        @Override
+        public void start() {
+            boolean lanternExists = this.fairyEntity.level.getBlockState(fairyEntity.lanternPos).getBlock()
+                    .equals(HexcraftBlocks.FAIRY_LANTERN.get());
+
+            if (lanternExists && fairyEntity.distanceToSqr(fairyEntity.lanternPos.getX() + 0.5,
+                    fairyEntity.lanternPos.getY() + 0.5, fairyEntity.lanternPos.getZ() + 0.5) > 2.0) {
+                this.fairyEntity.getMoveControl().setWantedPosition(fairyEntity.lanternPos.getX() + 0.5,
+                        fairyEntity.lanternPos.getY()  + 0.5, fairyEntity.lanternPos.getZ()  + 0.5, 1.0D);
             } else {
-                this.fairyEntity.getMoveControl().setWantedPosition(d0, d1, d2, 1.0D);
+                this.fairyEntity.lanternPos = null;
             }
+        }
+
+        public boolean canUse() {
+            return this.fairyEntity.lanternPos != null;
+        }
+
+        public boolean canContinueToUse() {
+            return false;
         }
     }
 }
