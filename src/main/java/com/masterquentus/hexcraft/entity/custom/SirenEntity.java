000b64dff3800e23698f1f53511747938a35d896
MasterQuentus <DaltonRpratt@gmail.com>
2023-06-20 18:36:11 -0500

    Working On SirenEntity

--- src/main/java/com/masterquentus/hexcraft/entity/custom/SirenEntity.java
+++ src/main/java/com/masterquentus/hexcraft/entity/custom/SirenEntity.java
@@ -70,6 +70,7 @@ public class SirenEntity extends Monster implements IAnimatable {
         this.targetSelector.addGoal(6, (new HurtByTargetGoal(this)).setAlertOthers());
     }
 
+
     private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
         if (event.isMoving()) {
             event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.siren.swim", true));
